268425ac5d06c90662240967142e0ccf4a22aefc
srava <srava@SHIVAMRAVAL>
2023-10-13 23:19:57 -0400

    Add project files.

---
+++ Triangle Test/TestAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TriangleTest
+{
+    internal class TestAttribute : Attribute
+    {
+    }
+}
---
+++ TriangleTest/Class1.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitDemonstration;
+
+namespace TriangleTest
+{
+    [TestFixture]
+
+    public class TriangleTest1
+    {
+        [Test]
+
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "The triangle is valid.";
+
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
